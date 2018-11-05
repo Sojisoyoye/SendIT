99d5333a26909158733aa2b5be8632e8e9ffe60e
Soji Soyoye <soji.soyoye@gmail.com>
2018-11-05 22:31:14 +0100

    Add contents to database and fix package.json

---
+++ parcels/db/db.js
@@ -0,0 +1,48 @@
+const parcels = [
+  {
+    id: '01',
+    userid: 1,
+    sendername: 'Sola Benson',
+    receivername: 'Joh Doe',
+    pickuplocation: 'Yaba',
+    destination: 'Lekki',
+    packagecontent: 'Document',
+    weight: '1.5kg',
+    price: '$2.0',
+  },
+  {
+    id: '02',
+    userid: 1,
+    sendername: 'Sola Benson',
+    receivername: 'Ade Olu',
+    pickuplocation: 'Gbagada',
+    destination: 'London',
+    packagecontent: 'Document',
+    weight: '2.5kg',
+    price: '$4.0',
+  },
+  {
+    id: '03',
+    userid: 2,
+    sendername: 'Bode John',
+    receivername: 'Ade Olu',
+    pickuplocation: 'Ota',
+    destination: 'Oyo',
+    packagecontent: 'Document',
+    weight: '2.5kg',
+    price: '$4.0',
+  },
+  {
+    id: '04',
+    userid: 3,
+    sendername: 'Chris Red',
+    receivername: 'David Paul',
+    pickuplocation: 'Agege',
+    destination: 'London',
+    packagecontent: 'Document',
+    weight: '2.5kg',
+    price: '$4.0',
+  },
+];
+
+export default parcels;
