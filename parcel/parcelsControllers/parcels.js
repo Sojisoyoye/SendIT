d5569a14aec1f7fb787b5814e17d94bd945570e5
Soji Soyoye <soji.soyoye@gmail.com>
2018-11-09 08:16:33 +0100

    fix eslint errors

--- parcel/parcelsControllers/parcels.js
+++ parcel/parcelsControllers/parcels.js
@@ -1,3 +1,4 @@
+/* eslint-disable class-methods-use-this */
 import db from '../db/db';
 import db1 from '../db/db1';
 
@@ -53,65 +54,65 @@ class ParcelsController {
   }
 
   createParcel(req, res) {
-    if(!req.body.userid) {
-        return res.status(400).send({
+    if (!req.body.userid) {
+      return res.status(400).send({
         success: 'false',
-        message: 'userid is required'
+        message: 'userid is required',
       });
-    } if(!req.body.sendername) {
-        return res.status(400).send({
+    } if (!req.body.sendername) {
+      return res.status(400).send({
         success: 'false',
-        message: 'sendername is required'
+        message: 'sendername is required',
       });
     } if (!req.body.receivername) {
-        return res.status(400).send({
+      return res.status(400).send({
         success: 'false',
-        message: 'receivername is required'    
-        });  
-    } else if (!req.body.pickuplocation) {
-      return res.status(400).send({
-      success: 'false',
-      message: 'pickuplocation is required'    
+        message: 'receivername is required',
       });
-    } else if (!req.body.destination) {
+    } if (!req.body.pickuplocation) {
       return res.status(400).send({
-      success: 'false',
-      message: 'destination is required'    
-      });  
-    } else if (!req.body.packagecontent) {
-      return res.status(400).send({
-      success: 'false',
-      message: 'packagecontent is required'    
+        success: 'false',
+        message: 'pickuplocation is required',
       });
-    } else if (!req.body.weight) {
+    } if (!req.body.destination) {
       return res.status(400).send({
-      success: 'false',
-      message: 'weight is required'    
+        success: 'false',
+        message: 'destination is required',
       });
-    } else if (!req.body.price) {
+    } if (!req.body.packagecontent) {
       return res.status(400).send({
-      success: 'false',
-      message: 'price is required'
+        success: 'false',
+        message: 'packagecontent is required',
+      });
+    } if (!req.body.weight) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'weight is required',
+      });
+    } if (!req.body.price) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'price is required',
       });
     }
 
-   const parcel = {
-     id: db.length + 1,
-     userid: req.body.userid,
-     sendername: req.body.sendername,
-     receivername: req.body.receivername,
-     pickuplocation: req.body.pickuplocation,
-     destination: req.body.destination,
-     packagecontent: req.body.packagecontent,
-     weight: req.body.weight,
-     price: req.body.price
-   }
-   db.push(parcel);
-   return res.status(201).send({
-   success: 'true',
-   message: 'parcel added successfully',
-   parcel
-   });
+    const parcel = {
+      id: db.length + 1,
+      userid: req.body.userid,
+      sendername: req.body.sendername,
+      receivername: req.body.receivername,
+      pickuplocation: req.body.pickuplocation,
+      destination: req.body.destination,
+      packagecontent: req.body.packagecontent,
+      weight: req.body.weight,
+      price: req.body.price,
+    }
+    db.push(parcel);
+    return res.status(201).send({
+      success: 'true',
+      message: 'parcel added successfully',
+      parcel,
+    });
   }
 
 
@@ -142,7 +143,7 @@ class ParcelsController {
         itemIndex = index;
       }
     });
-  
+
     if (!parcelFound) {
       return res.status(404).send({
         success: 'false',
@@ -150,65 +151,65 @@ class ParcelsController {
       });
     }
     if (!req.body.userid) {
-        return res.status(400).send({
-          success: 'false',
-          message: 'userid is required',
-        });
+      return res.status(400).send({
+        success: 'false',
+        message: 'userid is required',
+      });
     } if (!req.body.sendername) {
-        return res.status(400).send({
-          success: 'false',
-          message: 'sendername is required',
-        });
+      return res.status(400).send({
+        success: 'false',
+        message: 'sendername is required',
+      });
     } if (!req.body.receivername) {
-        return res.status(400).send({
-          success: 'false',
-          message: 'receivername is required',
-        });
-    } else if (!req.body.pickuplocation) {
-        return res.status(400).send({
-          success: 'false',
-          message: 'pickuplocation is required',
-        });
-    } else if (!req.body.destination) {
-        return res.status(400).send({
-          success: 'false',
-          message: 'destination is required',
-        });
-    } else if (!req.body.packagecontent) {
-        return res.status(400).send({
-          success: 'false',
-          message: 'packagecontent is required',
-        });
-    } else if (!req.body.weight) {
-        return res.status(400).send({
-          success: 'false',
-          message: 'weight is required',
-        });
-    } else if (!req.body.price) {
-        return res.status(400).send({
-          success: 'false',
-          message: 'price is required',
-        });
-    } 
-    
+      return res.status(400).send({
+        success: 'false',
+        message: 'receivername is required',
+      });
+    } if (!req.body.pickuplocation) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'pickuplocation is required',
+      });
+    } if (!req.body.destination) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'destination is required',
+      });
+    } if (!req.body.packagecontent) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'packagecontent is required',
+      });
+    } if (!req.body.weight) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'weight is required',
+      });
+    } if (!req.body.price) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'price is required',
+      });
+    }
+
     const updatedParcel = {
-        id: parcelFound.id,
-        userid: req.body.userid || parcelFound.userid,
-        sendername: req.body.sendername || parcelFound.sendername,
-        receivername: req.body.receivername || parcelFound.receivername,
-        pickuplocation: req.body.pickuplocation || parcelFound.pickuplocation,
-        destination: req.body.destination || parcelFound.destination,
-        packagecontent: req.body.packagecontent || parcelFound.packagecontent,
-        weight: req.body.weight || parcelFound.weight,
-        price: req.body.price || parcelFound.price,
+      id: parcelFound.id,
+      userid: req.body.userid || parcelFound.userid,
+      sendername: req.body.sendername || parcelFound.sendername,
+      receivername: req.body.receivername || parcelFound.receivername,
+      pickuplocation: req.body.pickuplocation || parcelFound.pickuplocation,
+      destination: req.body.destination || parcelFound.destination,
+      packagecontent: req.body.packagecontent || parcelFound.packagecontent,
+      weight: req.body.weight || parcelFound.weight,
+      price: req.body.price || parcelFound.price,
     };
-    
-      db.splice(itemIndex, 1, updatedParcel)
-    
+
+    db.splice(itemIndex, 1, updatedParcel)
+
     return res.status(201).send({
-        success: 'true',
-        message: 'Parcel added successfully',
-        updatedParcel,
+      success: 'true',
+      message: 'Parcel added successfully',
+      updatedParcel,
     });
   }
 }
