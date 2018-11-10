fecf52eae54449676e90995542f555ea0db7e20b
Soji Soyoye <soji.soyoye@gmail.com>
2018-11-10 14:52:39 +0100

    add database

---
+++ db/db.js
@@ -0,0 +1,26 @@
+const parcels = [
+  {
+    id: '01',
+    userid: 1,
+    sendername: 'Sola Benson',
+    receivername: 'Joh Doe',
+    pickuplocation: 'Yaba',
+    destination: 'Lekki',
+    packagecontent: 'Document',
+    weight: '1.5kg',
+    price: '$2.0',
+  },
+  {
+    id: '02',
+    userid: 1,
+    sendername: 'Sola Benson',
+    receivername: 'Ade Olu',
+    pickuplocation: 'Gbagada',
+    destination: 'London',
+    packagecontent: 'Document',
+    weight: '2.5kg',
+    price: '$4.0',
+  },
+];
+
+export default parcels;
