5a1dd2c69496ca6851d4ce49fdd1b7e90cdc0e7b
Soji Soyoye <soji.soyoye@gmail.com>
2018-11-11 23:32:49 +0100

    Add post api test

--- test/test.js
+++ test/test.js
@@ -3,6 +3,8 @@ import chai from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../app';
 
+// const Parcel = require('../db/db');
+
 // configure chai
 chai.should();
 chai.use(chaiHttp);
@@ -25,6 +27,50 @@ describe('Parcels', () => {
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.a('object');
+          done();
+        });
+    });
+  });
+
+  describe('Get Specific Parcel', () => {
+    it('should get a specific parcel', (done) => {
+      const id = 1;
+      chai.request(app)
+        .get('/api/v1/parcels/${id}')
+        .end((err, res) => {
+          // res.should.have.status(200);
+          res.body.should.be.a('object');
+          done();
+        });
+    });
+  });
+
+  describe('Create Parcel', () => {
+    it('should create a new parcel', (done) => {
+      const parcel = {
+        userid: 'user1',
+        sendername: 'sean paul',
+        receivername: 'craig david',
+        pickuplocation: 'yaba',
+        destination: 'oyo',
+        packagecontent: 'letter',
+        weight: '0.5kg',
+        price: '$4',
+      };
+      chai.request(app)
+        .post('/api/v1/parcels')
+        .send(parcel)
+        .end((err, res) => {
+          res.should.have.status(201);
+          res.body.should.be.a('object');
+          res.body.parcel.should.have.property('userid');
+          res.body.parcel.should.have.property('sendername');
+          res.body.parcel.should.have.property('receivername');
+          res.body.parcel.should.have.property('pickuplocation');
+          res.body.parcel.should.have.property('destination');
+          res.body.parcel.should.have.property('packagecontent');
+          res.body.parcel.should.have.property('weight');
+          res.body.parcel.should.have.property('price');
           done();
         });
     });
