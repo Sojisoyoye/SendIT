37600775240b466f2f6d97c2cf6bdccd32d20dd3
Sojisoyoye <soji.soyoye@gmail.com>
2018-11-23 00:29:23 +0100

    test input validators

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ import 'babel-polyfill';
 import Parcel from '../src/postgres/controllers/parcel';
 import User from '../src/postgres/controllers/user';
 import isValid from '../middleware/validations';
+import signUpValid from '../middleware/signupvalidator';
+import logInValid from '../middleware/loginvalidator';
 import Auth from '../src/postgres/middleware/Auth';
 import authAdmin from '../src/postgres/middleware/authAdmin';
 
@@ -13,13 +15,9 @@ dotenv.config();
 const router = express.Router();
 
 
-router.get('/', (req, res, next) => {
-  return res.status(200).json({ message: 'Welcome to SendIT' });
-});
+router.get('/', (req, res) => res.status(200).json({ message: 'Welcome to SendIT' }));
 
-router.get('/api/v1', (req, res, next) => {
-  return res.status(200).json({ message: 'Welcome to Version 1 of API' });
-});
+router.get('/api/v1', (req, res) => res.status(200).json({ message: 'Welcome to Version 1 of API' }));
 
 router.get('/api/v1/parcels', Auth.verifyToken, Parcel.getAll);
 
@@ -33,13 +31,13 @@ router.put('/api/v1/parcels/:id/destination', Auth.verifyToken, Parcel.updateDes
 
 router.put('/api/v1/parcels/:id/status', Auth.verifyToken, authAdmin.verifyAdmin, Parcel.updateStatus);
 
-router.put('api/v1/parcels/:id/currentlocation', Auth.verifyToken, authAdmin.verifyAdmin, Parcel.updateCurrentLocation);
+router.put('api/v1/parcels/:id/presentlocation', Auth.verifyToken, authAdmin.verifyAdmin, Parcel.updateCurrentLocation);
 
 router.delete('/api/v1/parcels/:id', Auth.verifyToken, Parcel.delete);
 
-router.post('/api/v1/users', User.create);
+router.post('/api/v1/auth/signup', signUpValid, User.create);
 
-router.post('/api/v1/users/login', User.login);
+router.post('/api/v1/auth/login', logInValid, User.login);
 
 
 export default router;
